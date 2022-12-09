2c8a45bd96c9bcb2fd0aa5757e1f22064fca660e
woosung1223 <dntjd991223@naver.com>
2022-12-09 17:08:32 +0900

    feat: 흐름을 제어하는 컨트롤러 생성

--- src/main/java/vendingmachine/controller/MachineController.java
+++ src/main/java/vendingmachine/controller/MachineController.java
@@ -1,14 +1,33 @@
 package vendingmachine.controller;
 
-import vendingmachine.domain.VendingMachine;
-import vendingmachine.view.InputView;
+import vendingmachine.domain.*;
+import vendingmachine.view.OutputView;
+
+import java.util.List;
 
 public class MachineController {
-    InputView inputView = new InputView();
-    VendingMachine vendingMachine;
+    private final InputController inputController = new InputController();
+    private final OutputView outputView = new OutputView();
+    private VendingMachine vendingMachine;
 
     public void run() {
-        inputView.readHoldingMoney();
 
+        Wallet wallet = inputController.readWallet();
+        wallet.makeMoneyToRandomCoins();
+        outputView.printCoins(wallet.getCoins());
+
+        List<Product> products = inputController.readProducts();
+
+        vendingMachine = new VendingMachine(wallet, products);
+
+        vendingMachine.putMoney(inputController.readUserMoney());
+
+        while (!vendingMachine.isServiceOver()) {
+            outputView.printCurrentMoney(vendingMachine.getCustomerMoney());
+            Product product = inputController.readProductToBuy();
+            vendingMachine.buy(product);
+        }
+
+        outputView.printChange(vendingMachine.getChange());
     }
 }
