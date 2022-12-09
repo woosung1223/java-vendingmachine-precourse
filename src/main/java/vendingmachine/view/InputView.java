66d59c2bf882db236c306f63ce8e17913851d2d3
woosung1223 <dntjd991223@naver.com>
2022-12-09 15:46:20 +0900

    refactor: 입력 메세지를 출력하도록 수정

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -8,11 +8,13 @@ import java.util.List;
 
 public class InputView {
     public int readHoldingMoney() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         String line = Console.readLine();
         return Integer.parseInt(line);
     }
 
     public List<List<String>> readProducts() {
+        System.out.println("상품명 가격, 수량을 입력해 주세요.");
         String line = Console.readLine();
         List<String> product = Arrays.asList(line.split(";"));
 
@@ -26,11 +28,13 @@ public class InputView {
     }
 
     public int readUserMoney() {
+        System.out.println("투입 금액을 입력해 주세요.");
         String line = Console.readLine();
         return Integer.parseInt(line);
     }
 
     public String readProductToBuy() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
         String line = Console.readLine();
         return line;
     }
