46e31a44352b6a50297b39b1371b549fcb125468
woosung1223 <dntjd991223@naver.com>
2022-12-09 14:47:18 +0900

    feat: 상품을 자판기가 보유하도록 작성

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -4,11 +4,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class VendingMachine {
-    private List<Coin> holdingCoins;
+    private final List<Coin> holdingCoins;
+    private final List<Product> products;
 
-    public VendingMachine(int holdingMoney) {
+    public VendingMachine(int holdingMoney, List<Product> products) {
         CoinConverter coinConverter = new CoinConverter();
+
         this.holdingCoins = coinConverter.convertToRandomCoins(holdingMoney);
+        this.products = products;
     }
 
     public List<Coin> getHoldingCoins() {
