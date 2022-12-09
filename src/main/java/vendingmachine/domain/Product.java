cda16e7d661dbdfdcf201fd126084fbdf73ce051
woosung1223 <dntjd991223@naver.com>
2022-12-09 14:53:13 +0900

    feat: 상품이 같은지 비교하는 기능 생성

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -1,5 +1,7 @@
 package vendingmachine.domain;
 
+import java.util.Objects;
+
 public class Product {
     private final String name;
     private final int price;
@@ -9,5 +11,23 @@ public class Product {
         this.name = name;
         this.price = price;
         this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Product)) {
+            return false;
+        }
+        Product product = (Product)other;
+        return Objects.equals(name, product.name) &&
+                price == product.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
