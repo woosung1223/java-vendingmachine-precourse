86071bfcda522918e28577b2965ebba1db842cfa
woosung1223 <dntjd991223@naver.com>
2022-12-09 16:12:50 +0900

    refactor: 입력 관련 메세지 상수화

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -7,14 +7,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
+    private final String READ_HOLDING_MONEY_MESSAGE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private final String READ_PRODUCTS_MESSAGE = "상품명 가격, 수량을 입력해주세요.";
+    private final String READ_USER_MONEY_MESSAGE = "투입 금액을 입력해 주세요.";
+    private final String READ_PRODUCTS_TO_BUY_MESSAGE = "구매할 상품명을 입력해 주세요.";
+
     public int readHoldingMoney() {
-        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+        System.out.println(READ_HOLDING_MONEY_MESSAGE);
         String line = Console.readLine();
         return Integer.parseInt(line);
     }
 
     public List<List<String>> readProducts() {
-        System.out.println("상품명 가격, 수량을 입력해 주세요.");
+        System.out.println(READ_PRODUCTS_MESSAGE);
         String line = Console.readLine();
         List<String> product = Arrays.asList(line.split(";"));
 
@@ -28,13 +33,13 @@ public class InputView {
     }
 
     public int readUserMoney() {
-        System.out.println("투입 금액을 입력해 주세요.");
+        System.out.println(READ_USER_MONEY_MESSAGE);
         String line = Console.readLine();
         return Integer.parseInt(line);
     }
 
     public String readProductToBuy() {
-        System.out.println("구매할 상품명을 입력해 주세요.");
+        System.out.println(READ_PRODUCTS_TO_BUY_MESSAGE);
         String line = Console.readLine();
         return line;
     }
