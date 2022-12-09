45ce75258c7fd49c7cbc4cfb58ad47a9e8dc6d08
woosung1223 <dntjd991223@naver.com>
2022-12-09 15:19:07 +0900

    feat: 잔돈을 반환하는 기능 생성

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -1,19 +1,19 @@
 package vendingmachine.domain;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class VendingMachine {
     private final List<Coin> holdingCoins;
     private final List<Product> products;
-    private int customerMoney = 0;
+    private int customerMoney = 0; // TODO: 감싸기
 
     public VendingMachine(int holdingMoney, List<Product> products) {
         CoinConverter coinConverter = new CoinConverter();
 
         this.holdingCoins = coinConverter.convertToRandomCoins(holdingMoney);
         this.products = products;
+
+        holdingCoins.sort((o1, o2) -> o2.getAmount() - o1.getAmount());
     }
 
     public List<Coin> getHoldingCoins() {
@@ -32,6 +32,16 @@ public class VendingMachine {
 
         productSelected.sell();
         customerMoney -= productSelected.getPrice();
+    }
+
+    public Map<Coin, Integer> getChange() {
+        Map<Coin, Integer> change = new HashMap<>();
+
+        for (Coin coin : holdingCoins) {
+            change.put(coin, customerMoney / coin.getAmount());
+            customerMoney %= coin.getAmount();
+        }
+        return change;
     }
 
     public boolean isServiceOver() {
