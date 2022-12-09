3a012f659f4fceef65a44308de4bd693003674b5
woosung1223 <dntjd991223@naver.com>
2022-12-09 14:18:35 +0900

    feat: 랜덤한 코인을 반환하는 기능 생성

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -1,4 +1,10 @@
-package vendingmachine;
+package vendingmachine.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public enum Coin {
     COIN_500(500),
@@ -13,4 +19,22 @@ public enum Coin {
     }
 
     // 추가 기능 구현
+    public static Coin getRandomCoin() {
+        List<Integer> amounts = Arrays.stream(values())
+                .map(coin -> coin.amount)
+                .collect(Collectors.toList());
+
+        return getCoinByAmount(Randoms.pickNumberInList(amounts));
+    }
+
+    public static Coin getCoinByAmount(int amount) {
+        return Arrays.stream(values())
+                .filter(coin -> coin.getAmount() == amount)
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 }
---
+++ src/main/java/vendingmachine/domain/CoinConverter.java
@@ -0,0 +1,10 @@
+package vendingmachine.domain;
+
+import java.util.List;
+
+public class CoinConverter {
+//    public List<Coin> convertToRandomCoins(int money) {
+//        // money 가 입력으로 들어오면 랜덤한 코인으로 바꾼다
+//
+//    }
+}
