3406e04bcfa629d4f3782015231fd53dfab3a4de
woosung1223 <dntjd991223@naver.com>
2022-12-09 18:11:14 +0900

    feat: Money에 대한 검증 절차 생성

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -4,6 +4,7 @@ public class Money {
     private int money;
 
     public Money(int money) {
+        validate(money);
         this.money = money;
     }
 
@@ -19,5 +20,20 @@ public class Money {
         return money;
     }
 
+    private void validate(int money) {
+        checkIsPositive(money);
+        checkIsDivisible(money);
+    }
 
+    private void checkIsPositive(int money) {
+        if (money <= 0) {
+            throw new IllegalArgumentException("[ERROR] 돈은 양수만 가능합니다.");
+        }
+    }
+
+    private void checkIsDivisible(int money) {
+        if (money % Coin.getSmallestAmount() != 0) {
+            throw new IllegalArgumentException("[ERROR] 1원 단위는 입력해서는 안됩니다.");
+        }
+    }
 }
