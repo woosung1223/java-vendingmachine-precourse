a4a0b8374f556fb1bc3f2025381a79fac8888f93
woosung1223 <dntjd991223@naver.com>
2022-12-09 14:48:48 +0900

    feat: 금액을 자판기에 넣는 기능 생성

--- src/main/java/vendingmachine/domain/VendingMachine.java
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class VendingMachine {
     private final List<Coin> holdingCoins;
     private final List<Product> products;
+    private int customerMoney = 0;
 
     public VendingMachine(int holdingMoney, List<Product> products) {
         CoinConverter coinConverter = new CoinConverter();
@@ -17,4 +18,10 @@ public class VendingMachine {
     public List<Coin> getHoldingCoins() {
         return Collections.unmodifiableList(holdingCoins);
     }
+
+    public void putMoney(int money) {
+        customerMoney = money;
+    }
+
+    
 }
